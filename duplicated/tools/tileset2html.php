b132eb5eceed10d8686a00a67875e570b6cbbd57
CYril Bèle <cyril.bele@gmail.com>
2018-11-01 15:26:06 +0100

    tileset with reverse images

--- duplicated/tools/tileset2html.php
+++ duplicated/tools/tileset2html.php
@@ -9,6 +9,13 @@ for($y=0;$y<4;$y++) {
 	}
 }
 
+echo '<div class="break"></div>'."\n";
+for($y=7;$y>=4;$y--) {
+	for($x=0;$x<8;$x++) {
+		echo '<div class="tile tile'.$x.'_'.$y.'" x="'.$x.'" y="'.$y.'" title="'.$x.'_'.$y.'"></div>'."\n";
+	}
+}
+
 for($x=0;$x<8;$x++) {
-	for($y=0;$y<4;$y++) echo '.tile'.$x.'_'.$y.'{background-position:-'.number_format($x*16,0).'px -'.number_format($y*16,0).'px;}'."\n";
+	for($y=0;$y<8;$y++) echo '.tile'.$x.'_'.$y.'{background-position:-'.number_format($x*16,0).'px -'.number_format($y*16,0).'px;}'."\n";
 }
